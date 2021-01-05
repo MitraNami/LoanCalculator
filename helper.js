80ff578165c4ebf9aa36faf333a444363a419c52
Mitra Nami <a.mxnami@gmail.com>
2021-01-05 00:22:53 -0500

    Factor out loan result calculation

--- helper.js
+++ helper.js
@@ -8,30 +8,43 @@ const calculateResults = function(evt) {
   const years = evt.target.querySelector('#years').value;
 
   //Make Sure all the fields have positive values
-  
-  
+
+
+
+  //Calculate the results
+  const principal = Number(loanAmount);
+  const interestRate = Number(interest) / 100;
+  const numberOfYears = Number(years);
+  const {
+    monthlyPayment,
+    totalPayment, 
+    totalInterest} = 
+  calculateLoanResults(principal, interestRate, numberOfYears);
+
+  //Dispaly the results
 
   //Get the elements into which you want to place the calculated values
   const monthlyPaymentEl = document.querySelector('#monthly-payment');
   const totalPaymentEl = document.querySelector('#total-payment');
   const totalInterestEl = document.querySelector('#total-interest');
 
-  //Calculate the results
-  const principal = Number(loanAmount);
-  const interestRate = Number(interest) / 100;
-  const numberOfyears = Number(years);
-
-  const x = Math.pow(1 + interestRate, numberOfyears);
-  const totalPayment = principal * x;
-  const totalInterest = totalPayment - principal;
-  const monthlyPayment = totalPayment / 12;
-
-  //Dispaly the results
   const resultsEl = document.querySelector('#results');
   //Reomve this element's d-none class
   resultsEl.classList.remove('d-none');
+
   //Put the calculated values into the input fields
-  monthlyPaymentEl.value = monthlyPayment;
-  totalPaymentEl.value = totalPayment;
-  totalInterestEl.value = totalInterest;
+  monthlyPaymentEl.value = monthlyPayment.toFixed(2);
+  totalPaymentEl.value = totalPayment.toFixed(2);
+  totalInterestEl.value = totalInterest.toFixed(2);
+};
+
+
+
+// Returns loan results based on the amount of money lent(principal), yearly interest rate and number of years you want to make payments
+const calculateLoanResults = function(principal, interestRate, numberOfYears) {
+  const x = Math.pow(1 + interestRate, numberOfYears);
+  const totalPayment = principal * x;
+  const totalInterest = totalPayment - principal;
+  const monthlyPayment = totalPayment / 12;
+  return {totalPayment, totalInterest, monthlyPayment};
 };

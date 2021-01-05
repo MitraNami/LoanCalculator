4ead60dd4179b1e267014056936380e5ce0b3485
Mitra Nami <a.mxnami@gmail.com>
2021-01-05 02:03:14 -0500

    Display an error message for invalid inputs

--- helper.js
+++ helper.js
@@ -7,9 +7,15 @@ const calculateResults = function(evt) {
   const interest = evt.target.querySelector('#interest').value;
   const years = evt.target.querySelector('#years').value;
 
-  //Make Sure all the fields have positive values
-
-
+  //Make Sure all the fields have positive values, interest is smaller than 100
+  if (loanAmount <= 0 || interest <= 0 || years <= 0) {
+    showError('All inputs must be greater than zero!');
+    return;
+  }
+  if (interest > 100) {
+    showError('Interest rate must be smaller than 100.');
+    return;
+  }
 
   //Calculate the results
   const principal = Number(loanAmount);
@@ -19,7 +25,7 @@ const calculateResults = function(evt) {
     monthlyPayment,
     totalPayment, 
     totalInterest} = 
-  calculateLoanResults(principal, interestRate, numberOfYears);
+  calculateLoanParameters(principal, interestRate, numberOfYears);
 
   //Dispaly the results
 
@@ -41,10 +47,35 @@ const calculateResults = function(evt) {
 
 
 // Returns loan results based on the amount of money lent(principal), yearly interest rate and number of years you want to make payments
-const calculateLoanResults = function(principal, interestRate, numberOfYears) {
+const calculateLoanParameters = function(principal, interestRate, numberOfYears) {
   const x = Math.pow(1 + interestRate, numberOfYears);
   const totalPayment = principal * x;
   const totalInterest = totalPayment - principal;
   const monthlyPayment = totalPayment / 12;
   return {totalPayment, totalInterest, monthlyPayment};
+};
+
+
+// Creates a div element to show the error message and remove it after 2 seconds
+const showError = function(msg) {
+  //Create a div
+  const errorEl = document.createElement('div');
+  errorEl.className = "alert alert-danger text-center";
+
+  //Create the text node and append it to the div
+  errorEl.appendChild(document.createTextNode(msg));
+
+  //Insert the errorEl below the heading
+  const heading = document.querySelector('.card-title');
+  heading.insertAdjacentElement('afterend', errorEl);
+
+  clearError();
+};
+
+
+//Removes the error element after 2 seconds
+const clearError = function() {
+  setTimeout(() => {
+    document.querySelector('.alert').remove();
+  }, 2000);
 };
